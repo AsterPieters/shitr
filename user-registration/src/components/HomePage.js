c4bc4c64775f6fa20d9e155088846dfa317c1e0c
aster pieters <aster.ieters@gmail.com>
2024-08-01 22:03:32 +0200

    made the homepage a bit nicer

--- user-registration/src/components/HomePage.js
+++ user-registration/src/components/HomePage.js
@@ -15,8 +15,17 @@ const HomePage = () => {
         <div id="posts-container">
             {posts.map((post, index) => (
                 <div key={index} className="post">
-                    <h2>{post.location}</h2>
-                    <p>{post.rating}/5</p>
+                    <h2>{post.username}</h2>
+                    <h3>{post.location}</h3>
+                    <h4>{post.post_name}</h4>
+
+                    <p>Comfort: {post.comfort}/5</p>
+                    <p>Smell: {post.smell}/5</p>
+                    <p>Aesthetic: {post.aesthetic}/5</p>
+                    <p>Toilet Paper: {post.toilet_paper}/5</p>
+                    <p>Overall Experience: {post.overall_experience}/5</p>
+                    
+                    <h5>Description: {post.description}</h5>
                 </div>
             ))}
         </div>
